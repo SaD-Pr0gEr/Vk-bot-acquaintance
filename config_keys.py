9f8a6c879d1b2b65fe696c4ba0a180f2b2ede9b1
Ozodchik <dilovardehkanov730@gmail.com>
2021-02-05 18:27:48 +0300

    commit

---
+++ config_keys.py
@@ -0,0 +1,5 @@
+user_token = "91188393b5a40f0c610ae6c26fcbdc825cc928945dfbf8117b03323942fde04d0fb175b9e842bf82c5fc7"
+bots_token = 'ed8e88703b96c5fd0b77dbb37fe2d76113a49ccfd7639d66214da340a22d98741185dad363f6d79661da8'
+owner_db = "owner_pro_diplom_db"
+db_name = "pro_diplom_db"
+db_password = "owner_pro_diplom_db"
